5afd794918cb2dde5a359b4755a923fa8645ce6d
Taitanos <hayaashi87@gmail.com>
2022-08-03 21:27:22 +0300

    create main page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello! Let's go!
-    </div>
-  );
+    return (
+        <div className="app-wrapper">
+            <header>
+                <img
+                    src={"https://static8.depositphotos.com/1378583/1010/i/600/depositphotos_10109073-stock-photo-flower-logo.jpg"}/>
+            </header>
+            <nav>
+                <div>Profile</div>
+                <div>Message</div>
+                <div>News</div>
+                <div>Music</div>
+                <div>Setting</div>
+            </nav>
+            <div>
+                Main content
+            </div>
+        </div>
+    );
 }
 
 export default App;
