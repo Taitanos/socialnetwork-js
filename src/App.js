278cb03929f68abc738dc43687dfd864e62b59b8
Taitanos <hayaashi87@gmail.com>
2022-08-06 15:31:42 +0300

    social network, grid

--- src/App.js
+++ src/App.js
@@ -3,20 +3,32 @@ import './App.css';
 
 function App() {
     return (
-        <div className="app-wrapper">
-            <header>
+        <div className={'app-wrapper'}>
+            <header className={'header'}>
                 <img
-                    src={"https://static8.depositphotos.com/1378583/1010/i/600/depositphotos_10109073-stock-photo-flower-logo.jpg"}/>
+                    src={'https://static8.depositphotos.com/1378583/1010/i/600/depositphotos_10109073-stock-photo-flower-logo.jpg'}/>
             </header>
-            <nav>
-                <div>Profile</div>
-                <div>Message</div>
-                <div>News</div>
-                <div>Music</div>
-                <div>Setting</div>
+            <nav className={'nav'}>
+                <div><a>Profile</a></div>
+                <div><a>Message</a></div>
+                <div><a>News</a></div>
+                <div><a>Music</a></div>
+                <div><a>Setting</a></div>
             </nav>
-            <div>
-                Main content
+            <div className={'content'}>
+                <div>
+                    <img src={'https://images.unsplash.com/photo-1616555846456-6d53b92669a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE0fHx8ZW58MHx8fHw%3D&w=1000&q=80'}/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    new post
+                </div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                </div>
             </div>
         </div>
     );
