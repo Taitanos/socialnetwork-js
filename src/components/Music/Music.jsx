34276d2aeed7823c938e96bb812e80129de6818f
Taitanos <hayaashi87@gmail.com>
2022-09-26 23:35:11 +0300

    add News.jsx, Setting.jsx and Music.jsx

---
+++ src/components/Music/Music.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Misuc.module.css'
+
+function Music() {
+    return (
+        <div>
+            Music
+        </div>
+    )
+}
+
+export default Music;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,11 +4,11 @@ import s from "./Navbar.module.css";
 function Navbar() {
     return (
         <nav className={s.nav}>
-            <div><a>Profile</a></div>
-            <div><a>Message</a></div>
-            <div><a>News</a></div>
-            <div><a>Music</a></div>
-            <div><a>Setting</a></div>
+            <div><a href={'profile'}>Profile</a></div>
+            <div><a href={'dialog'}>Message</a></div>
+            <div><a href={'news'}>News</a></div>
+            <div><a href={'music'}>Music</a></div>
+            <div><a href={'setting'}>Setting</a></div>
         </nav>
     )
 }
