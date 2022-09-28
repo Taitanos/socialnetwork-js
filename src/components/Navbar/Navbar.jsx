eea337ce04b668423d44b10451a61a128003c348
Taitanos <hayaashi87@gmail.com>
2022-09-28 22:22:31 +0300

    add NavLink, refactoring Navbar.module.css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import s from "./Navbar.module.css";
+import {NavLink} from "react-router-dom";
 
 function Navbar() {
     return (
         <nav className={s.nav}>
-            <div><a href={'profile'}>Profile</a></div>
-            <div><a href={'dialog'}>Message</a></div>
-            <div><a href={'news'}>News</a></div>
-            <div><a href={'music'}>Music</a></div>
-            <div><a href={'setting'}>Setting</a></div>
+            <div className={s.item}>
+                <NavLink to={'profile'} activeClassName={s.activeLink}>Profile</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to={'dialog'} activeClassName={s.activeLink}>Message</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to={'news'} activeClassName={s.activeLink}>News</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to={'music'} activeClassName={s.activeLink}>Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to={'setting'} activeClassName={s.activeLink}>Setting</NavLink>
+            </div>
         </nav>
     )
 }
