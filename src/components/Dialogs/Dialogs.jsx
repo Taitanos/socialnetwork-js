6372eb70f7e2e6a47157a0cfa3331e6c6d917e6c
Taitanos <hayaashi87@gmail.com>
2022-09-29 23:17:15 +0300

    made dialogue

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,29 @@ import s from './Dialogs.module.css'
 
 function Dialogs() {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={s.dialog}>
+                    Alexey
+                </div>
+                <div className={s.dialog}>
+                    Tatyana
+                </div>
+                <div className={s.dialog}>
+                    Sergey
+                </div>
+                <div className={s.dialog + '' + s.active}>
+                    Alina
+                </div>
+                <div className={s.dialog}>
+                    Ponteley
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>What are you doing here?</div>
+            </div>
         </div>
     )
 }
