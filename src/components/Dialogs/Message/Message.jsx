bb75ea606203741f0db9de58e354b38ecfe9aa5d
Taitanos <hayaashi87@gmail.com>
2023-03-09 20:20:07 +0300

    added css-style

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -4,7 +4,12 @@ import s from './Message.module.css'
 
 function Message(props) {
     return (
-        <div className={s.message}>{props.message}</div>
+        <div className={s.contentMessage}>
+            <div className={s.avaMessage}>
+                <img src={"https://weblinks.ru/wp-content/uploads/2022/08/0d6de7af1701b7f6ff551d4474ced401-730x578.jpeg"} alt={"Avatar"}/>
+            </div>
+            <div className={s.message}>{props.message}</div>
+        </div>
     )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -28,9 +28,9 @@ function Navbar() {
                     <div className={s.friends}> Friends
                         {/*Прокинуть аватарки друзей*/}
                         <div>
-                            <img src={"https://weblinks.ru/wp-content/uploads/2022/08/0d6de7af1701b7f6ff551d4474ced401-730x578.jpeg"}/>
-                            <img src={"https://weblinks.ru/wp-content/uploads/2022/08/0d6de7af1701b7f6ff551d4474ced401-730x578.jpeg"}/>
-                            <img src={"https://weblinks.ru/wp-content/uploads/2022/08/0d6de7af1701b7f6ff551d4474ced401-730x578.jpeg"}/>
+                            <img src={"https://weblinks.ru/wp-content/uploads/2022/08/0d6de7af1701b7f6ff551d4474ced401-730x578.jpeg"} alt={"Avatar"}/>
+                            <img src={"https://weblinks.ru/wp-content/uploads/2022/08/0d6de7af1701b7f6ff551d4474ced401-730x578.jpeg"} alt={"Avatar"}/>
+                            <img src={"https://weblinks.ru/wp-content/uploads/2022/08/0d6de7af1701b7f6ff551d4474ced401-730x578.jpeg"} alt={"Avatar"}/>
                         </div>
                     </div>
                 </div>
