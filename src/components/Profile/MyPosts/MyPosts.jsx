e6dc2d06ac6f9572e2518d83791f887684ef6334
Taitanos <hayaashi87@gmail.com>
2023-03-28 22:21:23 +0300

    added render.js

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -12,6 +12,7 @@ function MyPosts(props) {
         debugger;
         let text = newPostElement.current.value;
         props.addPost(text);
+        newPostElement.current.value = '';
     }
 
     return (
--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {rerenderEntireTree} from "./render";
 import state from "./redux/state";
-import {addPost} from "./redux/state";
-import {BrowserRouter} from "react-router-dom";
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App state={state} addPost={addPost}/>
-    </BrowserRouter>, document.getElementById('root')
-);
+rerenderEntireTree(state);
