439e2111bbe205e309f48e3860e5eb9edfd1844b
Taitanos <hayaashi87@gmail.com>
2023-04-18 18:27:43 +0300

    refactoring code, added dispatch and action

--- src/App.js
+++ src/App.js
@@ -18,14 +18,11 @@ function App(props) {
             <div className={'app-wrapper-content'}>
                 <Route path='/profile' render={() => <Profile
                     profilePage={props.state.profilePage}
-                    addPost={props.addPost}
-                    updateNewPostText={props.updateNewPostText}
+                    dispatch={props.dispatch}
                 />}/>
                 <Route path='/dialogs' render={() => <Dialogs
                     dialogsPage={props.state.dialogsPage}
-                    addMessage={props.addMessage}
-                    updateNewMessageText={props.updateNewMessageText}
-                    newMessageText={props.newMessageText}
+                    dispatch={props.dispatch}
                 />}/>
                 <Route path='/news' render={() => <News/>}/>
                 <Route path='/music' render={() => <Music/>}/>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -9,12 +9,12 @@ function MyPosts(props) {
     let newPostElement = React.createRef();
 
     let addPost = () => {
-        props.addPost();
+        props.dispatch ({type: 'ADD-POST'});
     }
 
     const onPostChange = () => {
         let text = newPostElement.current.value;
-        props.updateNewPostText(text);
+        props.dispatch ({type:'UPDATE-NEW-POST-TEXT', newText: text})
     }
 
     return (
