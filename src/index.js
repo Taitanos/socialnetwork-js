19c37c74a827f6e78ba3816169162579de0f15cb
Taitanos <hayaashi87@gmail.com>
2024-03-26 16:22:15 +0300

    47. copy of state in ProfileReducer

--- src/index.js
+++ src/index.js
@@ -6,21 +6,11 @@ import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 
-export let rerenderEntireTree = () => {
-    ReactDOM.render(
-        <BrowserRouter>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </BrowserRouter>
-        , document.getElementById('root')
-    );
-}
-
-rerenderEntireTree(/*store.getState()*/);
-
-
-store.subscribe(() => {
-/*    let state = store.getState();*/
-    rerenderEntireTree()
-})
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </BrowserRouter>
+    , document.getElementById('root')
+);
