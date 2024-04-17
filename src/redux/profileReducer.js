928448095e3ec507728710c682999fa55581496d
Taitanos <hayaashi87@gmail.com>
2024-04-17 16:40:51 +0300

    59 profile page, ajax, api

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,10 +1,10 @@
-
 let initialState = {
-        posts: [
-            {id: 1, message: 'Hi, it\'s my first post', likesCounter: 15},
-            {id: 2, message: 'Hi, how old are you?', likesCounter: 15},
-        ],
-        newPostText: ''
+    posts: [
+        {id: 1, message: 'Hi, it\'s my first post', likesCounter: 15},
+        {id: 2, message: 'Hi, how old are you?', likesCounter: 15},
+    ],
+    newPostText: '',
+    profile: null,
 }
 
 const profileReducer = (state = initialState, action) => {
@@ -23,12 +23,16 @@ const profileReducer = (state = initialState, action) => {
                 newPostText: action.newText,
             };
         }
+        case 'SET-USER-PROFILE': {
+            return {...state, profile: action.profile}
+        }
         default:
             return state;
     }
 }
 
 export const addPostActionCreator = () => ({type: 'ADD-POST'})
+export const setUserProfile = (profile) => ({type: 'SET-USER-PROFILE', profile})
 export const updateNewPostActionCreator = (text) => ({type: 'UPDATE-NEW-POST-TEXT', newText: text})
 
 export default profileReducer;
